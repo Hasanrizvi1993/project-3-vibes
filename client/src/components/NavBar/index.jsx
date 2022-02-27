b01fb1b8558d4be16cc8f57b7f0a6da177cec286
nabilk11 <khan.nabiln11@gmail.com>
2022-02-27 17:31:56 -0500

    added initial navbar html template - with logo text & links text for home & profile pages

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './navBar.scss';
 
+
+
 export const NavBar = () => {
   return (
-    <div>
-        NAVBAR COMPONENT
+    <div className='nav-container'>
+        <div className="nav-left">
+        <span className="logo">VIBE$</span>
+        </div>
+        <div className="nav-center">
+        <div className="searchbar">
+            
+           
+            </div>
+        </div>
+        <div className="nav-right">
+            <div className="nav-links">
+                <span className="nav-link">Home</span>
+                <span className="nav-link">Profile</span>
+            </div>
+        </div>
     </div>
   )
 }
