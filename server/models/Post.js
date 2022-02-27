ad4ee1dc5d361db2aef352cedcc57990ed18c04d
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-02-27 13:29:15 -0800

    worked on models of user and post

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,20 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    body: {type: String, required: true}
-})
+	body: { type: String, required: true },
+});
 
 const postSchema = new Schema(
-    {
-        title: {type: String, required: true},
-        body: {type: String, required: true},
-        date: {type: Date}, 
-        comments: [commentSchema]
-    }, 
-    {
-        timestamps: true
-    }
-)
+	{
+		_id: { type: Schema.Types.ObjectId },
+		name: { type: String, required: true },
+		userName: { type: String, required: true, unique: true },
+		comments: [commentSchema],
+		likes: [{ type: String }],
+		dislikes: [{ type: String }],
+		title: { type: String, required: true },
+		body: { type: String, required: true },
+		date: { type: Date },
+	},
+	{
+		timestamps: true,
+	},
+);
 
-module.exports = mongoose.model("Post", postSchema)
+module.exports = mongoose.model("Post", postSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,29 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-   firstName: {
-       type: String,
-       required: true,
-       unique: true,
-   },
-   lastName: {
-       type: String,
-       required: true,
-       unique: true,
-   },
-   email: {
-       type: String,
-       required: true,
-       unique: true,
-   },
-   password: {
-       type: String, 
-       required: true,
-       //specifies default path selection behavior aka false excludes "password" from query results by default
-       select: false
-   }
+const userProfileSchema = new Schema({
+	aboutMe: { type: String },
+	profileimage: { type: URL },
 });
+
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+
+		userName: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+
+		password: {
+			type: String,
+			required: true,
+			//specifies default path selection behavior aka false excludes "password" from query results by default
+			select: false,
+		},
+
+		currentlyPlaying: { type: String },
+
+		userProfile: [userProfileSchema],
+
+		location: {
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
