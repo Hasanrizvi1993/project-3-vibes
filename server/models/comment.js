c5d59b44081e16a1829df545c2076ca762e5e3c9
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-02-28 20:33:07 -0800

    comments full crud

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,8 +1,8 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const commentSchema = new Schema({
-// 	body: { type: String, required: true },
-// });
+const commentSchema = new Schema({
+	body: { type: String, required: true },
+});
 
-// module.exports = mongoose.model("Comment", commentSchema);
+module.exports = mongoose.model("Comment", commentSchema);
