11f5b7be3be7f6dcacada6cc82b13622fd74e5da
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-03-01 20:29:56 -0800

    made more changes

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,7 +4,6 @@ const { comments } = require("../controllers");
 router.get("/", comments.index);
 router.get("/:id", comments.show);
 // router.post("/posts/:id/comments", comments.create)
-// router.post("/posts/comments", comments.create)
 //above router.post needs to work and then the one below gets deleted
 router.post("/", comments.create);
 router.put("/:id", comments.update);
