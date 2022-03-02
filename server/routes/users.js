35f74806a300cdd819ce64fe6b73ae26cd0772af
joewhitneywalker <walker.joe81@gmail.com>
2022-03-01 19:34:51 -0800

    added users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const { users } = require("../controllers");
-const authRequired = require("../middleware/auth.required");
+// const authRequired = require("../middleware/auth.required"); COMMENTING OUT UNTIL MIDDLEWARE IS ADDED BACK IN
 
 router.get("/", users.index);
-router.get("/profile", authRequired, users.show);
+router.get("/profile",users.show); //RMOVED AUTHREQUIRED TO WAIT FOR AUTHENTICATION 
+router.put("/:id", users.update)
+router.delete("/:id", users.destroy)
+
+
 
 
 module.exports = router;
