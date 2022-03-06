e1bf0dbd91671469faed2706f5b8d295281d511b
joewhitneywalker <walker.joe81@gmail.com>
2022-03-05 17:48:56 -0800

    updating side bar styling

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -21,19 +21,18 @@ export const Sidebar = () => {
             <hr className='side-bar-divider'/>
             <div className="side-bar-center">
               <div  className="side-bar-label">NOW PLAYING</div>
-              <div><iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/playlist/1479458365" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
+              <div><iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/playlist/1479458365" width="200" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
 
                   </div>
-                <p className="current-song">NEED MUSIC PLAYER COMPONENT?</p>
             </div>
             <hr className='side-bar-divider'/>
             <div className="side-bar-bottom">
                 <div className="side-bar-label">TOP ARTISTS</div>
                 <ul className="side-bar-top-artists">
-                    <li className="top-artist">Billy Joel</li>
-                    <li className="top-artist">Fleetwood Mac</li>
-                    <li className="top-artist">The Eagles</li>
-                    <li className="top-artist">Lucius</li>
+                    <li className="top-artist">BILLY JOEL</li>
+                    <li className="top-artist">FLEETWOOD MAC</li>
+                    <li className="top-artist">THE EAGLES</li>
+                    <li className="top-artist">LUCIUS</li>
                 </ul>
             </div>
         </div>   
